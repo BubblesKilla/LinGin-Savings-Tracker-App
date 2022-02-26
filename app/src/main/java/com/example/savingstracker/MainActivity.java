c2db483b226aece67f7a4f30ba93d97b351e7a2f
Lindsey <lgarrigu@oswego.edu>
2022-02-26 04:53:38 -0500

    e

--- app/src/main/java/com/example/savingstracker/MainActivity.java
+++ app/src/main/java/com/example/savingstracker/MainActivity.java
@@ -2,7 +2,8 @@ package com.example.savingstracker;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.DialogInterface;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,13 +13,14 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity {
 
 ////////////////////////////// EACH VALUE HAS A FLOAT //////////////////////////////
-    public static float goalFloat = 20000;
+    public static float goalFloat;
     public static float bankFloat;
     public static float savingsFloat;
     public static float cashFloat;
     public static float totalFloat; // true total
     public static float displayFloat; // total - cash (for display purposes)
 ////////////////////////////// EACH VALUE HAS A STRING //////////////////////////////
+    public static String goalString;
     public static String bankString;
     public static String savingsString;
     public static String cashString;
@@ -30,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
     public TextView tVdisplayTotal;
     public TextView tVcash;
     public TextView tVtotal;
-
+    public TextView tVgoal;
+    public TextView tVgoalAmount;
+    public TextView tVgoalReached;
+//////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// GETTERS FOR THE TOTALS ///////////////////////////////
 
     float getTotalFloat () {
@@ -111,10 +116,24 @@ public class MainActivity extends AppCompatActivity {
 
         void whenReachedGoal(){
         // TODO: figure out what happens when we reach our goal
+            // new text: "you have reach your goal"
+            tVgoal.setVisibility(View.INVISIBLE);
+            tVgoalAmount.setVisibility(View.INVISIBLE);
+            tVgoalReached.setVisibility(View.VISIBLE);
+            updateCashString();
+
+            /** Ask the user if they want to restart the savings or continue from where they are.
+             *  If continue: nothing happens.
+             *  If restart:
+             *      > could restart from 0 and input new savings goal
+             *      > could restart from a set amount of $ and input new savings goal
+             */
+
         }
 
 
 
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -124,19 +143,42 @@ public class MainActivity extends AppCompatActivity {
         tVdisplayTotal = findViewById(R.id.totalAmountTV);
         tVcash = findViewById(R.id.cashAmountTV);
         tVtotal = findViewById(R.id.totalAmountTV);
+        tVgoal = findViewById(R.id.goalTV);
+        tVgoalAmount = findViewById(R.id.goalAmountTV);
+        tVgoalReached = findViewById(R.id.goalReachedTV);
+
         tVcash.setText("???");
+
+        // TODO create some sort of boolean that gets checked onCreate to see if goal reached
+        // not needed ASAP
+
+        goalFloat = 20000;
+        goalString = getString(R.string.goalAmount); // "$20,000"
+
+        if (getTotalFloat() < goalFloat) tVgoalReached.setVisibility(View.INVISIBLE);
+        else {
+            tVgoal.setVisibility(View.INVISIBLE);
+            tVgoalAmount.setVisibility(View.INVISIBLE);}
+
+        /**
+         * if we ask them for their new amount,
+         * they input like 20000 (INT NOT FLOAT FOR THE INPUT)
+         * and we figure out where the comma would need to go for goal string
+         */
+
 
         updateCashString();
         updateBankString();
         updateSavingsString();
         updateDisplayString();
 
-        findViewById(R.id.cashButton).setOnClickListener((View.OnClickListener) new HandleClick());
-        findViewById(R.id.bankButton).setOnClickListener((View.OnClickListener) new HandleClick());
-        findViewById(R.id.savingsButton).setOnClickListener((View.OnClickListener) new HandleClick());}
+        findViewById(R.id.cashButton).setOnClickListener(new HandleClick());
+        findViewById(R.id.bankButton).setOnClickListener(new HandleClick());
+        findViewById(R.id.savingsButton).setOnClickListener(new HandleClick());}
 
     private class HandleClick implements View.OnClickListener {
 
+        @SuppressLint("NonConstantResourceId")
         public void onClick(View arg0) {
 
             AlertDialog.Builder builder = new AlertDialog.Builder(arg0.getContext());
@@ -149,24 +191,22 @@ public class MainActivity extends AppCompatActivity {
 
                     builder.setView(userInput);
                     // set dialog message
-                    builder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog,int id) {
-                                            String uIn = userInput.getText().toString();
-                                            try {
-                                                float uCash = Float.parseFloat(uIn);
-                                                cashFloat += uCash;
-                                                totalFloat = getTotalFloat();
-                                                updateCashString();
-                                                updateTotalString();
-                                                Snackbar.make(getWindow().getDecorView().getRootView(),
-                                                        "$" + uIn + " added to total! ♥", Snackbar.LENGTH_LONG)
-                                                        .setAction("Action", null).show();
-                                            } catch (NumberFormatException e) {
-                                                Snackbar.make(getWindow().getDecorView().getRootView(),"um? thats not a number...",
-                                                        Snackbar.LENGTH_LONG)
-                                                        .setAction("Action", null).show();}}})
-                            .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                        public void onClick(DialogInterface dialog,int id) { dialog.cancel();}});
+                    builder.setCancelable(false).setPositiveButton("OK", (dialog, id) -> {
+                        String uIn = userInput.getText().toString();
+                        try {
+                            float uCash = Float.parseFloat(uIn);
+                            cashFloat += uCash;
+                            totalFloat = getTotalFloat();
+                            updateCashString();
+                            updateTotalString();
+                            Snackbar.make(getWindow().getDecorView().getRootView(),
+                                    "$" + uIn + " added to total! ♥", Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();
+                        } catch (NumberFormatException e) {
+                            Snackbar.make(getWindow().getDecorView().getRootView(),"um? thats not a number...",
+                                    Snackbar.LENGTH_LONG)
+                                    .setAction("Action", null).show();}})
+                            .setNegativeButton("Cancel", (dialog, id) -> dialog.cancel());
                 break;
 
             case R.id.bankButton:
@@ -174,23 +214,21 @@ public class MainActivity extends AppCompatActivity {
 
                 builder.setView(userInput);
                 // set dialog message
-                builder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        String uIn = userInput.getText().toString();
-                        try {
-                            bankFloat = Float.parseFloat(uIn);
-                            totalFloat = getTotalFloat();
-                            updateBankString();
-                            updateTotalString();
-                            Snackbar.make(getWindow().getDecorView().getRootView(),
-                                    "Checking Account set to $" + bankString + ".", Snackbar.LENGTH_LONG)
-                                    .setAction("Action", null).show();
-                        } catch (NumberFormatException e) {
-                            Snackbar.make(getWindow().getDecorView().getRootView(),"um? thats not a number...",
-                                    Snackbar.LENGTH_LONG)
-                                    .setAction("Action", null).show();}}})
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) { dialog.cancel();}});
+                builder.setCancelable(false).setPositiveButton("OK", (dialog, id) -> {
+                    String uIn = userInput.getText().toString();
+                    try {
+                        bankFloat = Float.parseFloat(uIn);
+                        totalFloat = getTotalFloat();
+                        updateBankString();
+                        updateTotalString();
+                        Snackbar.make(getWindow().getDecorView().getRootView(),
+                                "Checking Account set to $" + bankString + ".", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    } catch (NumberFormatException e) {
+                        Snackbar.make(getWindow().getDecorView().getRootView(),"um? thats not a number...",
+                                Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();}})
+                        .setNegativeButton("Cancel", (dialog, id) -> dialog.cancel());
                 break;
 
             case R.id.savingsButton:
@@ -198,23 +236,21 @@ public class MainActivity extends AppCompatActivity {
 
                 builder.setView(userInput);
                 // set dialog message
-                builder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog,int id) {
-                        String uIn = userInput.getText().toString();
-                        try {
-                            savingsFloat = Float.parseFloat(uIn);
-                            totalFloat = getTotalFloat();
-                            updateSavingsString();
-                            updateTotalString();
-                            Snackbar.make(getWindow().getDecorView().getRootView(),
-                                    "Savings Account set to $" + savingsString + ".", Snackbar.LENGTH_LONG)
-                                    .setAction("Action", null).show();
-                        } catch (NumberFormatException e) {
-                            Snackbar.make(getWindow().getDecorView().getRootView(),"um? thats not a number...",
-                                    Snackbar.LENGTH_LONG)
-                                    .setAction("Action", null).show();}}})
-                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog,int id) { dialog.cancel();}});
+                builder.setCancelable(false).setPositiveButton("OK", (dialog, id) -> {
+                    String uIn = userInput.getText().toString();
+                    try {
+                        savingsFloat = Float.parseFloat(uIn);
+                        totalFloat = getTotalFloat();
+                        updateSavingsString();
+                        updateTotalString();
+                        Snackbar.make(getWindow().getDecorView().getRootView(),
+                                "Savings Account set to $" + savingsString + ".", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    } catch (NumberFormatException e) {
+                        Snackbar.make(getWindow().getDecorView().getRootView(),"um? thats not a number...",
+                                Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();}})
+                        .setNegativeButton("Cancel", (dialog, id) -> dialog.cancel());
                 break;}
             AlertDialog alertDialog = builder.create();
             alertDialog.show();}}}
